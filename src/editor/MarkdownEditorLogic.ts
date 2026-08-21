dd18a9ecc92511a46bf14720b5ef81b7fc745011
jishuai <706631052@qq.com>
2026-08-21 17:38:53 +0800

    feat: UI scale up one-third, chrome weight 700
    Font tiers x4/3 (2xs 16 / xs 17 / sm 19 / md 20 / lg 21 / xl 23 / 2xl 27), chrome weight to 700 (real YaHei bold), containers resized to fit (menubar 44, footer 46, toolbar 52, buttons/inputs/search/tab bar up), default reading size 22px. DESIGN_SPEC appendix synced.

--- src/editor/MarkdownEditorLogic.ts
+++ src/editor/MarkdownEditorLogic.ts
@@ -59,8 +59,8 @@ export function createMarkdownEditorComponent(DCLogic, React) {
     this.immersiveWideRef = React.createRef();
     this.headerMoreRef = React.createRef();
     this.headerMenuRef = React.createRef();
-    this.fontSize = 17;
-    this.previewFontSize = 17;
+    this.fontSize = 22;
+    this.previewFontSize = 22;
     this.fontFamily = '';
     this.searchBarRef = React.createRef();
     this.searchInputRef = React.createRef();
